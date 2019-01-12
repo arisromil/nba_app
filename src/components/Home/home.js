bed12dc958bc229f20740f3a9b85d03bc4cc8590
arisromil <arisromil@yahoo.com>
2019-01-11 21:48:33 -0700

    React practice project
    React practice project

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
+import NewsSlider from '../widgets/NewsSlider/slider';
+import NewsList from '../widgets/NewsList/newsList';
+import  VideosList from '../widgets/VideosList/videosList';
+
+//npm start
+//npm run dev-serv
+
 const Home = () => {
     return(
         <div>
-            home
+            <NewsSlider
+              type="featured"
+              start={0}
+              amount={3}
+              settings={{
+                  dots:false
+              }}
+            />
+
+           <NewsList
+               type="card"
+               loadmore={true}
+               start={3}
+               amount={3}
+           />
+
+           <VideosList
+             type="card"
+             title={true}
+             loadmore={true}
+             start={0}
+             amount={3}
+           />
+
         </div>
     )
 }
